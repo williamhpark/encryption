9890b0637e91df529c41f8d7a6d2f5c7ed9d2839
William Park <w22park@uwaterloo.ca>
2022-10-14 13:57:33 +0100

    Basic implementation for AES

--- server/main.go
+++ server/main.go
@@ -9,4 +9,7 @@ import (
 func main() {
 	fmt.Println(methods.Caesar("test", 3, true))
 	fmt.Println(methods.Caesar("qbpq", 3, false))
+
+	fmt.Println(methods.AES("This is a secret", "thisis32bitlongpassphraseimusing", true))
+	fmt.Println(methods.AES("145149d64a1a3c4025e67665001a3167", "thisis32bitlongpassphraseimusing", false))
 }
--- server/methods/methods.go
+++ server/methods/methods.go
@@ -1,10 +1,15 @@
 package methods
 
+import (
+	"crypto/aes"
+	"encoding/hex"
+)
+
 // Caesar Cipher encryption/decryption
 // Params:
 // `input` - string to encrypt/decrypt
 // `key` - number of letters to shift by
-// `direction` - true to encrypt, false to decrypt
+// `encrypt` - true to encrypt, false to decrypt
 func Caesar(input string, key int, encrypt bool) string {
 	// Cast the string to an array of runes
 	runes := []rune(input)
@@ -15,7 +20,19 @@ func Caesar(input string, key int, encrypt bool) string {
 	offset := rune(26)
 
 	switch encrypt {
-	// Decoding
+	// Encrypt
+	case true:
+		for index, char := range runes {
+			if char >= 'a'+shift && char <= 'z' ||
+				char >= 'A'+shift && char <= 'Z' {
+				char = char - shift
+			} else if char >= 'a' && char < 'a'+shift ||
+				char >= 'A' && char < 'A'+shift {
+				char = char - shift + offset
+			}
+			runes[index] = char
+		}
+	// Decrypt
 	case false:
 		for index, char := range runes {
 			if char >= 'a' && char <= 'z'-shift ||
@@ -27,19 +44,58 @@ func Caesar(input string, key int, encrypt bool) string {
 			}
 			runes[index] = char
 		}
-	// Encoding
 	default:
-		for index, char := range runes {
-			if char >= 'a'+shift && char <= 'z' ||
-				char >= 'A'+shift && char <= 'Z' {
-				char = char - shift
-			} else if char >= 'a' && char < 'a'+shift ||
-				char >= 'A' && char < 'A'+shift {
-				char = char - shift + offset
-			}
-			runes[index] = char
-		}
+		panic("Unexpected value for `encrypt` argument")
 	}
 
 	return string(runes)
+}
+
+// Advanced Encryption Standard (AES) encryption/decryption
+// Params:
+// `input` - string to encrypt/decrypt
+// `s_key` - cipher key
+// `encrypt` - true to encrypt, false to decrypt
+func AES(input string, s_key string, encrypt bool) string {
+	// Cast the cipher key to an array of bytes
+	key := []byte(s_key)
+
+	switch encrypt {
+	// Encrypt
+	case true:
+		// Create cipher
+		c, err := aes.NewCipher(key)
+		if err != nil {
+			panic(err)
+		}
+
+		// Allocate space for ciphered data
+		out := make([]byte, len(input))
+
+		// Encrypt the string
+		c.Encrypt(out, []byte(input))
+
+		// Return the hexadecimal encoding for the string
+		return hex.EncodeToString(out)
+	// Decrypt
+	case false:
+		// Retrieve the bytes represented by the hexadecimal string
+		ciphertext, _ := hex.DecodeString(input)
+
+		// Create cipher
+		c, err := aes.NewCipher(key)
+		if err != nil {
+			panic(err)
+		}
+
+		// Allocate space for ciphered data
+		pt := make([]byte, len(ciphertext))
+
+		// Decrypt the string
+		c.Decrypt(pt, ciphertext)
+
+		return string(pt[:])
+	default:
+		panic("Unexpected value for `encrypt` argument")
+	}
 }
