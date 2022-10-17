18b35112b203101d418ce8a40b3efdb60bfc420a
William Park <w22park@uwaterloo.ca>
2022-10-18 00:33:58 +0100

    Update method descriptions

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -9,9 +9,12 @@ type HeaderProps = {
 
 const Header: React.FunctionComponent<HeaderProps> = ({ method }) => {
   const descriptions: { [key: string]: string } = {
-    [Method.Caesar]: "Caesar",
-    [Method.AES]: "AES",
-    [Method.RSA]: "RSA",
+    [Method.Caesar]:
+      "A Caesar cipher is a simple method of encoding messages. Caesar ciphers use a substitution method where letters in the alphabet are shifted by some fixed number of spaces to yield an encoding alphabet. A Caesar cipher with a shift of 1 would encode an A as a B, an M as an N, and a Z as an A, and so on.",
+    [Method.AES]:
+      "The Advanced Encryption Standard (AES) is a fast and secure form of encryption that keeps prying eyes away from our data. We see it in messaging apps like WhatsApp and Signal, programs like VeraCrypt and WinZip, in a range of hardware and a variety of other technologies that we use all of the time.",
+    [Method.RSA]:
+      "Under RSA encryption, messages are encrypted with a code called a public key, which can be shared openly. Due to some distinct mathematical properties of the RSA algorithm, once a message has been encrypted with the public key, it can only be decrypted by another key, known as the private key. Each RSA user has a key pair consisting of their public and private keys. As the name suggests, the private key must be kept secret.",
   };
 
   return (
